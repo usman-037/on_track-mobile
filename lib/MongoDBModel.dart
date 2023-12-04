8b3548bff3f4c74b4485e250860e13875b386c48
usman-037 <usmanbashir376@gmail.com>
2023-12-04 20:42:21 +0500

    implemented route registration

--- lib/MongoDBModel.dart
+++ lib/MongoDBModel.dart
@@ -1,9 +1,4 @@
-// To parse this JSON data, do
-//
-//     final mongoDbModel = mongoDbModelFromJson(jsonString);
-
 import 'dart:convert';
-
 import 'package:mongo_dart/mongo_dart.dart';
 
 MongoDbModel mongoDbModelFromJson(String str) => MongoDbModel.fromJson(json.decode(str));
@@ -13,34 +8,40 @@ String mongoDbModelToJson(MongoDbModel data) => json.encode(data.toJson());
 class MongoDbModel {
   ObjectId id;
   String name;
-  String phonenumber;
   String email;
   String password;
   String role;
+  int route;
+  String stop;
   MongoDbModel({
     required this.id,
     required this.name,
-    required this.phonenumber,
     required this.email,
     required this.password,
     required this.role,
+    required this.route,
+    required this.stop,
+
   });
 
   factory MongoDbModel.fromJson(Map<String, dynamic> json) => MongoDbModel(
     id: json["_id"],
     name: json["name"],
-    phonenumber: json["phonenumber"],
     email: json["email"],
     password: json["password"],
     role: json["role"],
+    route: json["route"],
+    stop: json["stop"],
+
   );
 
   Map<String, dynamic> toJson() => {
     "_id": id,
     "name": name,
-    "phonenumber": phonenumber,
     "email": email,
     "password": password,
     "role":role,
+    "route":route,
+    "stop":stop,
   };
 }
